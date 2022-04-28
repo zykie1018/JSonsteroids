5a41b95d6d45eda979f6687a72213b80651549cf
the25thbam <54788959+zykie1018@users.noreply.github.com>
2022-04-28 21:15:40 +0800

    Challenge 2: Cat Generator
    focused more on html and css
    (specifically flexbox)

--- static/js/script.js
+++ static/js/script.js
@@ -4,14 +4,24 @@ function ageInDays() {
     var birthYear = prompt('what year were you born?');
     // console.log(birthYear);
     var ageDays = (2022 - birthYear) * 365;
-    var h1 = document.createElement('h1');
+    var h1 = document.createElement('h1'); // creates an h1 element
     var textAnswer = document.createTextNode('You are ' + ageDays + ' days old');
-    h1.setAttribute('id', 'ageInDays');
-    h1.appendChild(textAnswer);
-    document.getElementById('flex-box-result').appendChild(h1);
+    h1.setAttribute('id', 'ageInDays'); // set this h1 tags id = ageInDays 
+    h1.appendChild(textAnswer); // append the var textanswer to h1 
+    document.getElementById('flex-box-result').appendChild(h1); // append h1 to id= flex-box-result
     // console.log(ageDays);
 }
 
 function reset() {
     document.getElementById('ageInDays').remove();
+}
+
+
+// Challenge 2: Cat Generator
+
+function generateCat() {
+    var image = document.createElement('img');
+    var div = document.getElementById('flex-cat-gen');
+    image.src = "https://thecatapi.com/api/images/get?format=src&type=gif&size=small";
+    div.appendChild(image);
 }
